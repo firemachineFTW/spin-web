ca31a9f1fdf5fc3960c41d0bc62f96db377a578c
Juan Jose Mendoza Becerril <juanjo_mendoza130@outlook.com>
2024-04-02 17:41:38 -0600

    commit-ruta-myv-2

--- src/routes/MisionVision.jsx
+++ src/routes/MisionVision.jsx
@@ -6,11 +6,11 @@ const MisionVision = () => {
     <div className='container'>
       <div className='container-mision-vision'>
         <div className='container-mision'>
-            <h1>MISION</h1>
+            <h1 className='tittle-mision'>MISION</h1>
             <p className='txt-mision'>Ofrecer soluciones efectivas y sobresalientes, asumiendo las necesidades de los clientes como nuestras</p>
         </div>
         <div className='container-vision'>
-          <h1>VISION</h1>
+          <h1 className='tittle-vision'>VISION</h1>
           <p className='txt-vision'>Posicionar a Soluciones SPIN como la empresa líder ofreciendo la mejor oferta comercial del mercado</p>
         </div>
       </div>
